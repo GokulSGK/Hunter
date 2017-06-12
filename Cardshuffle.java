ea76adee34cd3470c3433f5379d328d8fd471095
GokulSGK <gokul.dinesh1997@gmail.com>
2017-06-12 21:23:09 +0530

    Create Cardshuffle.java

---
+++ Cardshuffle.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Cardshuffle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("size");
+		int n = s.nextInt();
+		ArrayList <Integer> al = new ArrayList<Integer>();
+		for(int i=0;i<n;i++)
+		{
+			int m = s.nextInt();
+			al.add(m);
+		}
+		Random j = new Random(3);
+		Collections.shuffle(al, j);
+		for(Integer o:al)
+		{
+			System.out.println(o);
+		}
+
+	}
+
+}
