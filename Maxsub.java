ee115c16b5b9b9e47c9a0100d891cebb0553608b
GokulSGK <gokul.dinesh1997@gmail.com>
2017-06-13 07:04:36 +0530

    Create Maxsub.java

---
+++ Maxsub.java
@@ -0,0 +1,48 @@
+package Vegeta;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Maxsub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String s1 = s.next();
+		int n = s1.length();
+		int m=0;
+		ArrayList <String> al = new ArrayList<String>();
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=i;j<n-1;j++)
+			{
+				String s2 = (s1.substring(i, j+1));
+				StringBuffer sb = new StringBuffer(s2);
+				if(s2.equals(sb.reverse()))
+			{
+				continue;
+			}
+				else
+				{
+					al.add(s2);
+				}
+			}
+		}
+		
+		int length = 1;
+		for(int i = 0;i<al.size();i++)
+		{
+			String k = al.get(i);
+			if(k.length() > length)
+			{
+				m = i;
+				length = k.length();
+			}
+		}
+		
+		System.out.println("Largest substring of "+al.get(m)+" "+al.get(m).length());
+		
+	}
+
+}
